0a4c33215e4c1a13db2469aaef3f63e00a7362c6
DeZin7 <marcusandre77@icloud.com>
2024-03-09 13:58:40 -0800

    Fixing acceptance test.

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-response=$(wget --quiet --no-check-certificate https://172.18.0.1:2376/sum\?a\=1\&b\=2)
+response=$(curl -sk https://172.18.0.1:8765/sum\?a\=1\&b\=2)
 echo "Response: $response"
 if [ "$response" = "3" ]; then
     echo "Test passed!"
